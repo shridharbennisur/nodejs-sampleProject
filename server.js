8e30b1333ab7467b77d58175fc9c5430a40a5a09
din <shridharmbennisur77@gmail.com>
2018-02-10 19:47:14 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', ()=> {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (message)=> {
+    return message.toUpperCase();
+});
+
+app.use((req, res, next)=> {
+    var now = new Date().toString();
+    var log = now + ' request metod: ' + req.method + ' request url: ' + req.url; 
+    console.log(log);
+    fs.appendFile('server.logs', log + '\n', (err)=> {
+        if (err) {
+            console.log('could not be append in server.logs');
+        }
+    });
+    next();
+});
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        'pageHeading': 'Home Page',
+        'welcomeMessage': 'Welcome to my website'
+    });
+});
+
+app.get('/about', (req, res)=> {
+    res.render('about.hbs', {
+        'pageHeading': 'About Page',
+    });
+});
+app.listen(3000, ()=> {
+    console.log('server running on 3000 port');
+});
